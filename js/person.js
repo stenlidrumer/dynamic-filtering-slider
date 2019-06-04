6e6090d328eccb600dda79ef41482f68714603eb
Stanislav Andreev <35640836+stenlidrumer@users.noreply.github.com>
2019-06-04 22:40:39 +0100

    Add files via upload

---
+++ js/person.js
@@ -0,0 +1,46 @@
+  var people = [
+    {
+      name: 'DeWalt DCD778L2T-SFGB', 
+      rate: 40
+    },
+    {
+      name: 'DeWalt DCD776S2T-GB', 
+      rate: 65
+    },
+    {
+      name: 'DeWalt DCD776D2T- GB', 
+      rate: 70
+    },
+    {
+      name: 'Makita DHP480SYE', 
+      rate: 75
+    },
+    {
+      name: 'Milwaukee M18CBLPD',
+      rate: 80
+    },
+    {
+      name: 'DeWalt DCD778D2T-GB',
+      rate: 85
+    },
+    {
+      name: 'Erbauer ECD18-Li-2',
+      rate: 90
+    },
+    {
+      name: 'Makita DHP453SFEW',
+      rate: 95
+    },
+    {
+      name: 'Mac Allister MSCD18',
+      rate: 100
+    },
+    {
+      name: 'DeWalt DCD996P2',
+      rate: 110
+    },
+    {
+      name: 'HiKOKI DV18DGL',
+      rate: 120
+    }
+  ];
